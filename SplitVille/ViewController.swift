681a9013c1355e04ea6ead10188363bca2f0f311
Adel Araji <adel_araji@Adels-MacBook-Pro.local>
2016-02-27 18:07:01 -0500

    Adel login screen

--- SplitVille/ViewController.swift
+++ SplitVille/ViewController.swift
@@ -9,15 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
 
+    @IBOutlet weak var VenmoButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
