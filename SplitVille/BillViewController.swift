db4c3a88b5f7218d4105b1acff771b18541e6016
Nabil Maadarani <nabil.maadarani@hotmail.com>
2016-02-28 13:08:12 -0500

    Added BillViewController.

---
+++ SplitVille/BillViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BillViewController.swift
+//  SplitVille
+//
+//  Created by Nabil Maadarani on 2016-02-28.
+//  Copyright © 2016 Nabil. All rights reserved.
+//
+
+import UIKit
+
+class BillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
